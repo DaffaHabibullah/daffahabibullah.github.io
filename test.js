d5933a58c9735cea3834de021489c16a6993a500
DaffaHabibullah <daffahabibullah1@gmail.com>
2022-09-16 23:09:34 +0700

    add style, button, and adjust

--- test.js
+++ test.js
@@ -27,7 +27,7 @@
 //         id: data.length + 1,
 //         img: "./public/Screenshot_5.png",
 //         judul: "gacha",
-//         harga: 10000
+//         count: 10000
 //     })
 
 //     for(let i = 0; i < data.length-1; i++){
@@ -41,100 +41,154 @@
 //         let card = document.createElement("div");
 //         let img = document.createElement("img");
 //         let judul = document.createElement("h1");
-//         let harga = document.createElement("p");
+//         let count = document.createElement("p");
 //         let btn = document.createElement("button");
         
 //         //set attribute img
 //         img.setAttribute('src', element.img);
 //         judul.innerText = element.judul;
-//         harga.innerText = element.harga;
+//         count.innerText = element.count;
 //         btn.innerText = "gatcha now!";
     
 //         //set classname
 //         card.className = "card1";
 //         img.className = "img";
 //         judul.className = "judul";
-//         harga.className = "harga";
+//         count.className = "count";
 //         btn.className = "btn";
     
 //         document.body.appendChild(card);
 //         card.appendChild(img);
 //         card.appendChild(judul);
-//         card.appendChild(harga);
+//         card.appendChild(count);
 //         card.appendChild(btn);
 //     });
 // })
 
-let tambah = [{
-    id: 1,
-    img: "./public/Screenshot_5.png",
-    judul: "gacha",
-    harga: 10000
+let tambah = [
+//     {
+//     id: 1,
+//     img: "./public/Screenshot_5.png",
+//     judul: "gacha",
+//     count: 10000
 
-},
-{
-    id: 2,
-    img: "./public/Screenshot_5.png",
-    judul: "gacha",
-    harga: 10000
-}]
+// },
+// {
+//     id: 2,
+//     img: "./public/Screenshot_5.png",
+//     judul: "gacha",
+//     count: 10000
+// }
+]
 
-tambah.map(element => {
+// tambah.map(element => {
+//     // let card = document.createElement("div");
+//     // let img = document.createElement("img");
+//     // let judul = document.createElement("h1");
+//     // let count = document.createElement("p");
+//     // let btn = document.createElement("button");
+    
+//     //set attribute img
+//     img.setAttribute('src', element.img);
+//     judul.innerText = element.judul;
+//     count.innerText = element.count;
+//     btn.innerText = "gatcha now!";
+
+//     //set classname
+//     card.className = "card1";
+//     img.className = "img";
+//     judul.className = "judul";
+//     count.className = "count";
+//     btn.className = "btn";
+
+//     document.body.appendChild(card);
+//     card.appendChild(img);
+//     card.appendChild(judul);
+//     card.appendChild(count);
+//     card.appendChild(btn);
+// })
+
+let bunttonTambah1 = document.querySelector("#buttonTambah1");
+bunttonTambah1.addEventListener("click", function(){
+    alert("r u sure?");
+    tambah.push({
+        id: tambah.length + 1,
+        img: "./public/Screenshot_5.png",
+        judul: "Congrats!", 
+        count: tambah.length + 1
+    })
+
     let card = document.createElement("div");
     let img = document.createElement("img");
     let judul = document.createElement("h1");
-    let harga = document.createElement("p");
-    let btn = document.createElement("button");
-    
-    //set attribute img
-    img.setAttribute('src', element.img);
-    judul.innerText = element.judul;
-    harga.innerText = element.harga;
-    btn.innerText = "gatcha now!";
+    let count = document.createElement("p");
+    let prize = document.createElement("p");
+    let btn1 = document.createElement("button");
+    let btnx = document.createElement("button");
+    let btn10 = document.createElement("button");
 
-    //set classname
+    btn1 = bunttonTambah1;
+    btn10 = bunttonTambah10;
+
     card.className = "card1";
-    img.className = "img";
-    judul.className = "judul";
-    harga.className = "harga";
-    btn.className = "btn";
+
+    img.setAttribute('src', "./public/Wish x1.jpg");
+    judul.innerText = tambah[tambah.length - 1].judul;
+    count.innerText = `Wish ${tambah[tambah.length - 1].count}`;
+    prize.innerText = "You got 1x 5* Weapon";
+    btn1.innerText = "x1";
+    btnx.innerText = "x";
+    btn10.innerText = "x10";
 
     document.body.appendChild(card);
     card.appendChild(img);
     card.appendChild(judul);
-    card.appendChild(harga);
-    card.appendChild(btn);
+    card.appendChild(count);
+    card.appendChild(prize);
+    card.appendChild(btn1);
+    card.appendChild(btnx);
+    card.appendChild(btn10);
 })
 
-let bunttonTambah = document.querySelector("#buttonTamba");
 
-bunttonTambah.addEventListener("click", function(){
+let bunttonTambah10 = document.querySelector("#buttonTambah10");
+bunttonTambah10.addEventListener("click", function(){
+    alert("r u sure?");
     tambah.push({
         id: tambah.length + 1,
         img: "./public/Screenshot_5.png",
-        judul: "gacha", 
-        harga: tambah.length + 10000
+        judul: "Congrats!", 
+        count: tambah.length + 1
     })
 
-    
     let card = document.createElement("div");
     let img = document.createElement("img");
     let judul = document.createElement("h1");
-    let harga = document.createElement("p");
-    let btn = document.createElement("button");
+    let count = document.createElement("p");
+    let prize = document.createElement("p");
+    let btn1 = document.createElement("button");
+    let btnx = document.createElement("button");
+    let btn10 = document.createElement("button");
+
+    btn1 = bunttonTambah1;
+    btn10 = bunttonTambah10;
 
     card.className = "card1";
 
     img.setAttribute('src', "./public/Screenshot_5.png");
     judul.innerText = tambah[tambah.length - 1].judul;
-    harga.innerText = `Rp.${tambah[tambah.length - 1].harga}`;
-    btn.innerText = "gatcha now!";
+    count.innerText = `Wish ${tambah[tambah.length - 1].count}`;
+    prize.innerText = "You got 1x 5* Character, 1x 4* Character, etc";
+    btn1.innerText = "x1";
+    btnx.innerText = "x";
+    btn10.innerText = "x10";
 
     document.body.appendChild(card);
     card.appendChild(img);
     card.appendChild(judul);
-    card.appendChild(harga);
-    card.appendChild(btn);
-    console.log(tambah.length);
+    card.appendChild(count);
+    card.appendChild(prize);
+    card.appendChild(btn1);
+    card.appendChild(btnx);
+    card.appendChild(btn10);
 })
-    
